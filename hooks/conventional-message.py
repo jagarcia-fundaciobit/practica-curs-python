7742d680ee47f8dd1468138d11021c338cdfc728
Juan Antonio Garcia <jagarcia@fundaciobit.org>
2024-03-25 14:01:49 +0100

    feat(web): correccio modul web

--- hooks/conventional-message.py
+++ hooks/conventional-message.py
@@ -5,18 +5,39 @@ import sys
 
 def main():
     regex = r"^\w+(\(.+\))*: .+"
-    commit_msg_filepath = sys.argv[1]
+    commit_msg_filepath = sys.argv[
+        1
+    ]
 
-    with open(commit_msg_filepath, "r") as file:
-        content = file.read().strip()
+    with open(
+        commit_msg_filepath,
+        "r",
+    ) as file:
+        content = (
+            file.read().strip()
+        )
 
-    if not re.match(regex, content):
-        print("ERROR: El missatge de commit no és convencional.")
+    if not re.match(
+        regex,
+        content,
+    ):
+        print(
+            "ERROR: El missatge de commit no és convencional."
+        )
         return 1
 
-    print("El teu missatge de commit és convencional.")
-    return 0
+    print(
+        "El teu missatge de commit és convencional."
+    )
+    return (
+        0
+    )
 
 
-if __name__ == "__main__":
-    sys.exit(main())
+if (
+    __name__
+    == "__main__"
+):
+    sys.exit(
+        main()
+    )
